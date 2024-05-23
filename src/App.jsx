e96abf42248540bf71a9dbe49c0589cd1aee9e71
github_username <nedimmuminovic61@gmail.com>
2024-05-23 12:54:15 +0200

    setting up some ground work

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ function App() {
   
 
   return (
-    <p>.</p>
+    <main>
+      <div id="game-container">
+          <ol id="players">
+            <li id="player-name">
+              <span>Player1</span>
+               <span id="player-symbol">X</span>
+            </li>
+
+            <li id="player-name">
+              <span>Player2</span>
+               <span id="player-symbol">O</span>
+            </li>
+          </ol>
+      </div>
+    </main>
   )
 }
 
